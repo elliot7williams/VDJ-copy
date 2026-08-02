0d872ddfdd26734e05f2984d283728b0b83ee018
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:13:24 -0400

    Add VDJ copy/VDJTests/VDJTests.swift

---
+++ VDJ copy/VDJTests/VDJTests.swift
@@ -0,0 +1,17 @@
+//
+//  VDJTests.swift
+//  VDJTests
+//
+//  Created by Elliot Williams on 2025-06-24.
+//
+
+import Testing
+@testable import VDJ
+
+struct VDJTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
